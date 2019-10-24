edce9f6aa3330477d351f848dd0eefb1465a2cae
庄嘉炜 <zhuangjiawei@bingosoft.net>
2019-10-24 11:17:56 +0800

    添加获取一定范围内的随机数（int,double）操作工具类

---
+++ src/main/java/com/zjw/common/utils/calculate/RandomUtils.java
@@ -0,0 +1,37 @@
+package com.zjw.common.utils.calculate;
+
+import java.util.Random;
+
+/**
+ * @version 1.0
+ * @Author: zjw
+ * @Description: 产生一定范围内的随机数
+ * @Date: Created in   2019/10/24 11:07
+ * @Modified By:
+ * @since 1.0
+ */
+public class RandomUtils {
+    /**
+     * 在[min,max]产生随机数
+     * @param min
+     * @param max
+     * @return
+     */
+    public static int getRandomInt(int min,int max){
+        Random rand = new Random();
+        int randNumber = rand.nextInt(max - min + 1) + min;
+        return randNumber;
+    }
+
+    /**
+     * 在[min,max]产生随机数  
+     * @param min
+     * @param max
+     * @return
+     */
+    public static Double getRandomDouble(Double min,Double max){
+        Random rand = new Random();
+        Double randNumber = rand.nextDouble()* (max - min) + min;
+        return randNumber;
+    }
+}
---
+++ src/test/java/com/zjw/common/utils/calculate/RandomUtilsTest.java
@@ -0,0 +1,28 @@
+package com.zjw.common.utils.calculate;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @version 1.0
+ * @Author: zjw
+ * @Description: com.zjw.common.utils.calculate
+ * @Date: Created in   2019/10/24 11:09
+ * @Modified By:
+ * @since 1.0
+ */
+class RandomUtilsTest {
+    @Test
+    void test(){
+        for(int i=0;i<50;i++){
+            int num= RandomUtils.getRandomInt(3,12);
+            assertTrue(num<=12&&num>=3);
+        }
+        for(int i=0;i<50;i++){
+            double num= RandomUtils.getRandomDouble(10D,13D);
+            assertTrue(DoubleCalcUtils.doubleCompare(num,10D)>=0&&DoubleCalcUtils.doubleCompare(num,20D)<=0);
+        }
+    }
+
+}
